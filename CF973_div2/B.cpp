07abe03a3868683957ec4fd7a816a8a405058d4a
qhk <2507227617@qq.com>
2024-09-22 23:35:24 +0800

    AC B

---
+++ CF973_div2/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    int n;
+    cin >> n;
+    LL ans = 0;
+    for (int i=1; i<=n; i++) {
+        int x;  cin >> x;
+        if (i == n - 1)  ans -= x;
+        else ans += x;
+    }
+    cout << ans << '\n';
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // scanf("%d",&T);
+    cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
