9ca127878cb415c576d8bb1f1929ed3f5a2ea832
qhk <2507227617@qq.com>
2024-09-27 22:16:13 +0800

    AC A

--- CF975_div1/A.cpp
+++ CF975_div1/A.cpp
@@ -4,7 +4,26 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
-    
+    LL n, m;
+    cin >> n >> m;
+    vector<LL> a(n+10, 0);
+    LL mx = 0, sum = 0;
+    for (int i=1; i<=n; i++) {
+        cin >> a[i];
+        mx = max(mx, a[i]);
+        sum += a[i];
+    }
+    LL ans = (sum + m) / mx;
+    ans = min(ans, n);
+    if (ans >= n) {
+        ans = n;
+    }
+    mx = (sum + m) / ans;
+    while (ans * mx < sum) {
+        mx += (sum - ans * mx - 1) / ans + 1;
+        ans = (sum + m) / mx;
+    }
+    cout << ans << '\n';
 }
 int main()
 {
@@ -14,7 +33,7 @@ int main()
     #endif
     ios::sync_with_stdio(false); cin.tie(0); 
     int T=1;
-    // cin >> T;
+    cin >> T;
     while(T--){
         work();
     }
