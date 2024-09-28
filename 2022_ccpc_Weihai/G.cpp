1a4c9a0bcc1b82836731f2dac82fdb03e4e0a2f7
qhk <2507227617@qq.com>
2024-09-28 14:53:17 +0800

    AC G

--- 2022_ccpc_Weihai/G.cpp
+++ 2022_ccpc_Weihai/G.cpp
@@ -4,7 +4,48 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
-    
+    LL x, n;
+    cin >> x >> n;
+    LL y = 1;
+    while (y <= x) {
+        y <<= 1;
+    }
+    if (x == 1) {
+        while (n --) {
+            LL l, r;
+            cin >> l >> r;
+            cout << r - l + 1 << '\n';
+        }
+        return;
+    }
+    if (x % 2 == 0) {
+        while (n --) {
+            LL l, r;  cin >> l >> r;
+            cout << 0 << '\n';
+        }
+        return;
+    }
+    vector<int> bo(y+10, 0);
+    for (int i=1; i<=y; i++) {
+        LL p = i * x % y;
+        p &= x;
+        if (__gcd(p, x) == 1) {
+            bo[i] = 1;
+        }
+        bo[i] += bo[i-1];
+        // cout << bo[i] << ' ';
+    }
+    // cout << '\n';
+    // cout << bo[y] << '\n';
+    while (n --) {
+        LL l, r;
+        cin >> l >> r;
+        l --;
+        LL ansr = bo[y] * (r / y) + bo[r % y];
+        LL ansl = bo[y] * (l / y) + bo[l % y];
+        LL ans = ansr - ansl;
+        cout << ans << '\n';
+    }
 }
 int main()
 {
