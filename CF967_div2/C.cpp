43f9daa7cff3deaeb071e6298d4b53bcc876c03d
qhk <2507227617@qq.com>
2024-10-01 00:24:39 +0800

    AC C

--- CF967_div2/C.cpp
+++ CF967_div2/C.cpp
@@ -4,18 +4,37 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
-    
+    int n;
+    cin >> n;
+    vector<int> f(n+10, 1);
+    for (int i=2; i<=n; i++) {
+        int fa = f[i];
+        while (true) {
+            cout << "? " << fa << " " << i << endl;
+            int x;  cin >> x;
+            if (x == fa) {
+                f[i] = x;
+                break;
+            }
+            fa = x;
+        }
+    }
+    cout << "! ";
+    for (int i=2; i<=n; i++) {
+        cout << f[i] << ' ' << i << ' ';
+    }
+    cout << endl;
 }
 int main()
 {
-    #ifdef QHK
-    freopen("qi.in","r",stdin);
-    freopen("qi.out","w",stdout);
-    #endif
+    // #ifdef QHK
+    // freopen("qi.in","r",stdin);
+    // freopen("qi.out","w",stdout);
+    // #endif
     ios::sync_with_stdio(false); cin.tie(0); 
     int T=1;
     // scanf("%d",&T);
-    // cin >> T;
+    cin >> T;
     while(T--){
         work();
     }
