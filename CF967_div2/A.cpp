354830d47eb6ffc24b8acc1953d8cb590ff171c8
qhk <2507227617@qq.com>
2024-09-30 23:17:48 +0800

    AC A

---
+++ CF967_div2/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    int n;
+    cin >> n;
+    vector<int> num(n+10, 0);
+    int ans = 0;
+    for (int i=1; i<=n; i++) {
+        int x;  cin >> x;
+        num[x] ++;
+        ans = max(ans, num[x]);
+    }
+    ans = n - ans;
+    cout << ans << '\n';
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // scanf("%d",&T);
+    cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
