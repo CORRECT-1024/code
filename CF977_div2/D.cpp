3bedfdfea1663817e751e6455ab70cac00e42667
qhk <2507227617@qq.com>
2024-10-09 23:16:37 +0800

    0

--- CF977_div2/D.cpp
+++ CF977_div2/D.cpp
@@ -4,7 +4,28 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<LL> > a(n+10, vector<LL> (m+10)), sum(a), fum(a);
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=m; j++) {
+            cin >> a[i][j];
+            sum[i][j] = sum[i][j-1] + a[i][j];
+        }
+        for (int j=n; j>=1; j--) {
+            fsum[i][j] = fsum[i][j+1] + a[i][j];
+        }
+    }
+    vector<vector<LL> > dp(2, vector<LL> (m+10, 0)), fdp(dp);
+    for (int i=1; i<=n; i++) {
+        LL mi = 0, p = i % 2;
+        for (int j=1; j<=m; j++) {
+            dp[p][j] = sum[i][j] - mi;
+            mi = min(mi, sum[i][j-1]);
+        }
         
+    }
+
 }
 int main()
 {
@@ -15,7 +36,7 @@ int main()
     ios::sync_with_stdio(false); cin.tie(0); 
     int T=1;
     // scanf("%d",&T);
-    // cin >> T;
+    cin >> T;
     while(T--){
         work();
     }
