175721ada395c66b521927fd27ecb26834472b86
qhk <2507227617@qq.com>
2024-10-09 23:24:06 +0800

    AC A

---
+++ Edu162/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    int n;
+    cin >> n;
+    vector<int> a(n+10, 0);
+    for (int i=1; i<=n; i++) {
+        cin >> a[i];
+    }
+    if (n != 2 || (abs(a[2] - a[1]) == 1)) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // scanf("%d",&T);
+    cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
