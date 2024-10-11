2a6258c679b0c38ef162a25958aa1b305a1eb9f4
qhk <2507227617@qq.com>
2024-10-12 00:45:06 +0800

    0

---
+++ CF966_div3/F.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    int n, k;
+    cin >> n >> k;
+    
+    // int sum = 0;
+    // LL ans = 0;
+    // priority_queue<pair<int, int>, vector<pair<int, int>>, less<pair<int, int> > > pri;
+    // for (int i=1; i<=n; i++) {
+    //     int a, b;  cin >> a >> b;
+    //     if (a > b) swap(a, b);
+    //     pri.push({a, b});
+    //     sum += a + b;
+    //     ans += a * b;
+    // }
+    // sum -= k;
+    // while (sum) {
+    //     int a = pri.top().first, b = pri.top().second;
+    //     pri.pop();
+    //     if ()
+    // }
+
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
