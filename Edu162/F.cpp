ea12b959e0381a72c0e1617511a67b24a4a4a2d1
qhk <2507227617@qq.com>
2024-10-11 23:08:28 +0800

    好麻烦，不想写

--- Edu162/F.cpp
+++ Edu162/F.cpp
@@ -1,3 +1,26 @@
 //ans = zong - ((lenlr为奇数时)ai 和 sumlr 同奇偶) - (ai为中心时，ai左边和ai右边相同sum的次数)
-//O(n^2) ? 
+//O(n^2log) ? 
 //困了，明天再写
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
--- test.cpp
+++ test.cpp
@@ -4,7 +4,15 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
-    
+    int n;
+    cin >> n;
+    double a = 0, b = 0;
+    for (int i=1; i<=n; i++) {
+        double x, y;  cin >> x >> y;
+        a += x * y;
+        b += y;
+    }
+    cout << a / b << '\n';
 }
 int main()
 {
