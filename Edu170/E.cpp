d8829e4f84c583d2ebe4b402534e9f50a371279c
qhk <2507227617@qq.com>
2024-10-16 00:13:05 +0800

    AC E

--- Edu170/E.cpp
+++ Edu170/E.cpp
@@ -15,13 +15,63 @@ void work() {
             p[j] %= mod;
         }
     }
-    for (int i=0; i<=m/2; i++) {
-        //拿几张牌，需要几张1牌，方案数
-        cout << i << ' ' << m - 2 * i << ' ' << p[i] << '\n';
+    // for (int i=0; i<=m/2; i++) {
+    //     //拿几张牌，需要几张1牌，方案数
+    //     cout << i << ' ' << m - 2 * i << ' ' << p[i] << '\n';
+    // }
+    // return;
+    vector<LL> dp(m+10, 0);
+    dp[0] = 1;
+    for (int i=2; i<=n; i++) {
+        vector<LL> fdp(m+10, 0);
+        for (int d = 0; d <= m / 2; d ++) {
+            //d是拿的牌数，fd是差值
+            int fd = (m - d) - d;
+            for (int be = 0; be <= m; be ++) {
+                if (be + fd > m) break;
+                fdp[be + fd] += dp[be] * p[d];
+                fdp[be + fd] %= mod;
+            }
+        }
+        swap(dp, fdp);
+        // for (int i=0; i<=m; i++) {
+        //     cout << dp[i] << ' ';
+        // }
+        // cout << '\n';
     }
-    return;
-    
-    
+    // for (int i=0; i<=m; i++) {
+    //     cout << dp[i] << ' ';
+    // }
+    // cout << '\n';
+    LL ans = 0;
+    vector<LL> q(m+10, 0);
+    q[0] = 1;
+    for (int i=0; i<=m; i++) {
+        //i:差值  fid：拿几张
+        // cout << i << ": ";
+        if ((m - i) % 2) continue;
+        int fid = i + (m - i) / 2;
+        q = vector<LL> (m+10, 0);
+        q[0] = 1;
+        for (int j=1; j<=m; j++) {
+            for (int l = j; l>=1; l--) {
+                if (l - i <= j - l) {
+                    q[l] += q[l - 1];
+                    q[l] %= mod;
+                }
+            }
+        }
+        // cout << i << ":\n";
+        // cout << fid << '\n';
+        // for (int j=0; j<=m; j++) {
+        //     cout << q[j] << ' ';
+        // }
+        // cout << '\n';
+        // cout << dp[i] << ' ' << q[fid] << '\n';
+        ans = (ans + dp[i] * q[fid] % mod) % mod;
+    }
+    // cout << '\n';
+    cout << ans << '\n';
 }
 int main()
 {
