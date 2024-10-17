b0a451e4e16fe9ad844a1af9f30c8f9e7b9ed498
qhk <2507227617@qq.com>
2024-10-18 00:06:58 +0800

    0

---
+++ codeforces/CF978_div2/C.cpp
@@ -0,0 +1,25 @@
+//读错题了，sad
+//简单但麻烦的dp，不想写
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
