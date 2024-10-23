6b52e399e463c7b5f8769f9da9ba3276be98663c
qhk <2507227617@qq.com>
2024-10-24 00:21:14 +0800

    0

---
+++ codeforces/CF980_div2/D.cpp
@@ -0,0 +1,25 @@
+//看到tag上的最短路想到了解法
+//其实自己也往图论上想了，甚至想过会不会向后移到各 个位置然后全向前，但是图论想了一会生成树（生成链？），想偏了
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    cin >> T;
+    while(T--){
+        work();
+    }
+
+   return 0;
+}
