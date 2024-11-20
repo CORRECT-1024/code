e6648730a58e88cfb92617d05f7df14ea36e16f3
qhk <2507227617@qq.com>
2024-11-21 00:18:44 +0800

    AC E

--- codeforces/CF979_div2/E.cpp
+++ codeforces/CF979_div2/E.cpp
@@ -3,6 +3,8 @@ using namespace std;
 typedef long long LL;
 typedef unsigned long long ULL;
 const int mod = 998244353;
+const int N = 2e5 + 10;
+LL jc[N];
 
 LL power(LL a, LL n) {
     LL ans = 1;
@@ -14,16 +16,25 @@ LL power(LL a, LL n) {
     return ans;
 }
 LL c(LL n, LL m) {
-    LL p = 1;
-    for (int i=0; i<n; i++) {
-        p = p * (m - i) % mod;
+    LL ans = jc[m] * power(jc[m-n], mod - 2) % mod;
+    ans = ans * power(jc[n], mod - 2) % mod;
+    return ans;
+    // LL p = 1;
+    // for (int i=0; i<n; i++) {
+    //     p = p * (m - i) % mod;
+    // }
+    // LL q = 1;
+    // for (int i=1; i<=n; i++) {
+    //     q = q * i % mod;
+    // }
+    // p = p * power(q, mod - 2) % mod;
+    // return p;
+}
+void init() {
+    jc[0] = 1;
+    for (int i=1; i<N-5; i++) {
+        jc[i] = jc[i-1] * i % mod;
     }
-    LL q = 1;
-    for (int i=1; i<=n; i++) {
-        q = q * i % mod;
-    }
-    p = p * power(q, mod - 2) % mod;
-    return p;
 }
 void work() {
     int n;
@@ -32,25 +43,28 @@ void work() {
     for (int i=1; i<=n; i++) {
         cin >> a[i];
     }
-    vector<vector<LL> > f(n+10, vector<LL> ());
     vector<int> num(n+10, 0);
     for (int i=1; i<=n; i++) {
-        num[a[i]+1] ++;
+        num[a[i]] ++;
     }
+    vector<LL> f(n+10, 0);
     LL f0 = power(2, n);
     for (int i=1; i<=n; i++) {
-        f[0].push_back(f0);
+        f[i] = f0;
     }
     int lanum = n;
     LL ans = 0;
-    for (int i=1; i<=n; i++) {
+    for (int i=0; i<n; i++) {
+        if (num[i] == 0) break;
         LL sum = power(2, num[i]), p = sum;
-        for (int j=0; j<min(num[i], lanum); j++) {
-            LL fp = f[i-1][j];
-            sum = (sum + mod - c(j, num[i])) % mod;
-            fp = (fp * fp) % mod * power(sum, mod - 2) % mod;
+        for (int j=1; j<=min(num[i], lanum); j++) {
+            LL fp = f[j];
+            sum = (sum + mod - c(j-1, num[i])) % mod;
+            fp = fp * power(p, mod - 2) % mod;
+            fp = fp * sum % mod;
             ans = (ans + fp) % mod;
-            f[i].push_back(fp);
+            // cout << sum << ' ' << ans << '\n';
+            f[j] = fp;
         }
         lanum = min(lanum, num[i]);
     }
@@ -63,6 +77,7 @@ int main()
     freopen("qi.out","w",stdout);
     #endif
     ios::sync_with_stdio(false); cin.tie(0); 
+    init();
     int T=1;
     cin >> T;
     while(T--){
