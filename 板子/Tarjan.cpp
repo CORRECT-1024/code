f2cbbaa56aaf3bcf76c595259801e1e3034311c7
qhk <2507227617@qq.com>
2024-11-29 00:06:30 +0800

    add Tarjan

---
+++ 板子/Tarjan.cpp
@@ -0,0 +1,29 @@
+vector<int> sz;
+vector<vector<int> > scc;
+
+void tarjan(int x, int dfncnt, vector<vector<int> > &bian, vector<int> &dfn, vector<int> &low, stack<int> &st, vector<int> inst) {
+    dfncnt ++;  dfn[x] = dfncnt;  low[x] = dfncnt;
+    st.push(x);  inst[x] = 1;
+    for (auto &to : bian[x]) {
+        if (!dfn[to] ) {
+            tarjan(to, dfncnt, bian, dfn, low, st, inst);
+            low[x] = min(low[x], low[to]);
+        } else if (inst[to] ) {
+            low[x] = min(low[x], low[to]);
+        }
+    }
+    if (dfn[x] == low[x] ) {
+        scc.push_back(vector<int>());
+        while (inst[x] ) {
+            scc[scc.size()-1].push_back(st.top());
+            inst[st.top()] = 0;
+            st.pop();
+        }
+    }
+}
+void tarjanfun(int n) {
+    sz = vector<int> (n+10, 0);
+    vector<int> dfn(n+10, 0), low(dfn), inst(dfn);
+    stack<int> st;
+    tarjan(1, 0, bian, dfn, low, st, inst);
+}
