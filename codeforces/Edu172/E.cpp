fa0133bbe73aca605587e1f9dd62964e4ff81a16
qhk <2507227617@qq.com>
2024-12-03 00:36:20 +0800

    0

---
+++ codeforces/Edu172/E.cpp
@@ -0,0 +1,29 @@
+// 树剖线段树 + lcm + 贪心
+// 我怎么现在还不会树剖？？？？
+// 该怎么确定第一个要删的点？？
+// 删掉一部分(k)后,枚举删掉的i, 另一个i到没被删的k的路径赋0(意为不能删)
+// 再把k和上一个k的路径赋0
+// 从权值大的(2*n)枚举到1,如xds[i]==1就能删,否则不能删.
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+
+void work() {
+    
+}
+int main()
+{
+    #ifdef QHK
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
+    #endif
+    ios::sync_with_stdio(false); cin.tie(0); 
+    int T=1;
+    // cin >> T;
+    while(T--) {
+        work();
+    }
+
+   return 0;
+}
