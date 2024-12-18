e2bf116f319772c4d5c382b085734ae57147af42
qhk <2507227617@qq.com>
2024-12-18 13:43:47 +0800

    0

--- 1.sh
+++ 1.sh
@@ -1,50 +1,110 @@
-./Project1 ./Instances/rand-2-30-15-306-233-0_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-1_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-2_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-3_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-4_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-5_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-6_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-7_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-8_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-9_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-10_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-11_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-12_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-13_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-14_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-15_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-16_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-17_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-18_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-19_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-20_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-21_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-22_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-23_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-24_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-25_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-26_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-27_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-28_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-29_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-30_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-31_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-32_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-33_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-34_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-35_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-36_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-37_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-38_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-39_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-40_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-41_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-42_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-43_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-44_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-45_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-46_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-47_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-48_ext-merged.xml
-./Project1 ./Instances/rand-2-30-15-306-233-49_ext-merged.xml
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-0_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-1_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-2_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-3_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-4_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-5_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-6_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-7_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-8_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--50/rand-2-25-20-300-171-fcd-9_ext-merged.xml ./ans/25-20-300--50.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-0_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-1_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-2_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-3_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-4_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-5_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-6_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-7_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-8_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--40/rand-2-25-20-300-181-fcd-9_ext-merged.xml ./ans/25-20-300--40.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-0_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-1_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-2_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-3_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-4_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-5_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-6_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-7_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-8_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--30/rand-2-25-20-300-191-fcd-9_ext-merged.xml ./ans/25-20-300--30.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-0_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-1_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-2_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-3_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-4_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-5_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-6_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-7_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-8_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--20/rand-2-25-20-300-201-fcd-9_ext-merged.xml ./ans/25-20-300--20.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-0_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-1_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-2_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-3_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-4_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-5_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-6_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-7_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-8_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300--10/rand-2-25-20-300-211-fcd-9_ext-merged.xml ./ans/25-20-300--10.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-0_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-1_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-2_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-3_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-4_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-5_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-6_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-7_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-8_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-0/rand-2-25-20-300-221-fcd-9_ext-merged.xml ./ans/25-20-300-0.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-0_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-1_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-2_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-3_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-4_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-5_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-6_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-7_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-8_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-10/rand-2-25-20-300-231-fcd-9_ext-merged.xml ./ans/25-20-300-10.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-0_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-1_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-2_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-3_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-4_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-5_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-6_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-7_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-8_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-20/rand-2-25-20-300-241-fcd-9_ext-merged.xml ./ans/25-20-300-20.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-0_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-1_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-2_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-3_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-4_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-5_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-6_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-7_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-8_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-30/rand-2-25-20-300-251-fcd-9_ext-merged.xml ./ans/25-20-300-30.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-0_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-1_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-2_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-3_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-4_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-5_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-6_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-7_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-8_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-40/rand-2-25-20-300-261-fcd-9_ext-merged.xml ./ans/25-20-300-40.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-0_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-1_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-2_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-3_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-4_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-5_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-6_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-7_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-8_ext-merged.xml ./ans/25-20-300-50.txt
+./Project1 ./Instances/25-20-300-50/rand-2-25-20-300-271-fcd-9_ext-merged.xml ./ans/25-20-300-50.txt
--- ICPC/2024_LatinAmerican/A.cpp
+++ ICPC/2024_LatinAmerican/A.cpp
@@ -4,16 +4,13 @@ typedef long long LL;
 typedef unsigned long long ULL;
 
 void work() {
-    for (int i=-50; i<=50; i+=10) {
-        for (int j=0; j<10; j++) {
-            cout << "./Project1 ./Instances/25-20-300-" << i << "/rand-2-25-20-300-" << 221 + i << "-fcd-" << j << "_ext-merged.xml ./ans/25-20-300-" << i <<".txt\n";
-        }
-    }
+    
 }
 int main()
 {
     #ifdef QHK
-    freopen("1.sh","w",stdout);
+    freopen("qi.in","r",stdin);
+    freopen("qi.out","w",stdout);
     #endif
     ios::sync_with_stdio(false); cin.tie(0); 
     int T=1;
